899102664f6357e31ab53484cbf8843ade6060c2
thegrims <grimshawaidan@gmail.com>
2016-05-05 15:09:14 -0700

    added some new stuff

--- Roll a Ball/Assets/scripts/changeScene.cs
+++ Roll a Ball/Assets/scripts/changeScene.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class changeScene : MonoBehaviour {
-    
+    public Image im;
     // Use this for initialization
     public void ChangeToScene(int scenetochangeto)
     {
-
+        for (int i=0; i<10; i++)
+        {
+            Debug.Log("blah");
+             im.GetComponent<CanvasRenderer>().SetAlpha(i/10);
+        }
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         //SceneManager.LoadScene(scenetochangeto, SceneManager.mode = LoadSceneMode.Single);
         //button.CrossFadeAlpha(1f, .1f, false);
